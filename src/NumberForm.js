6ef01a50f4e8fec8bf3148aac10fb21fd004305e
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-02-21 10:48:12 +0900

    数字以外除外のフォーム

---
+++ src/NumberForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+// 数字入力コンポーネント
+export default class NumberForm extends React.Component{
+    constructor (props){
+        super(props)
+        this.state = {value:''}
+    }
+    // 値が変更されたとき
+    doChange (e) {
+        const curValue = e.target.value
+        // 数字以外は削除
+        const newValue = curValue.replace(/[^0-9]/g,'')
+        this.setState({value:newValue})
+    }
+    doSubmit (e) {
+        alert(`値を送信：${this.state.value}`)
+        e.preventDefault()
+    }
+
+    // 画面の描画
+    render () {
+        //イベントをメソッドにバインド
+        const doSubmit = (e) => this.doSubmit(e)
+        const doChange = (e) => this.doChange(e)
+        return(
+            <form onSubmit={doSubmit}>
+                <input type="text" value = {this.state.value} onChange={doChange}/>
+                <input type="submit" value='送信'/>
+            </form>
+        )
+    }
+}
